94665b7efca789ad15eade73dab30d238a374ba3
MrGreat-0 <jagjeetsinghrana2@gmail.com>
2025-01-31 21:01:52 +0530

    remove &enablejsapi=1 from yt link

--- src/components/Trailer.jsx
+++ src/components/Trailer.jsx
@@ -33,7 +33,7 @@ const Trailer = () => {
             <ReactPlayer
               width="100%"
               height="100%"
-              url={`https://www.youtube.com/watch?v=${trailer.key}&enablejsapi=1`}
+              url={`https://www.youtube.com/watch?v=${trailer.key}`}
             />
           ) : (
             <div className="w-full h-full grid place-items-center">
