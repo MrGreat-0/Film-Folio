a184cf3b3442225714ad7bc80f014d6308db3227
MrGreat-0 <jagjeetsinghrana2@gmail.com>
2025-02-04 16:53:24 +0530

    Create ScrollToTop.jsx
    Create ScrollToTop.jsx and add in App.jsx , every time navigate to a new page, it will automatically scroll to the top

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Routing from "./utils/Routing";
 import Nav from "./components/templates/Header/Nav";
 import Footer from "./components/templates/Footer/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 const App = () => {
   return (
     <main className="w-full h-full bg-[#1F1E24] text-zinc-200 relative">
+      <ScrollToTop />
       <Nav />
       <Routing />
       <Footer />
